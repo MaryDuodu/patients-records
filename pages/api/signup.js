490320fffaa8648481ba8d9e7deb7a23c6dc3492
Lawrence Ephrim <ephrimlawrence@gmail.com>
2022-05-08 18:30:22 +0000

    added unique username to signup api

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -4,12 +4,20 @@ export default async function handler(req, res) {
     const { db } = await connectToDatabase()
 
     const name = req.body.name
-    const email = req.body.email
+    const username = req.body.username
     const password = req.body.password
 
-    await db.collection("users").insertOne({name: name,
-        email, password})
-    res.status(200).json({ name, email: email, password });
+    const user = await db.collection("users").findOne({ username: username })
+
+    if (user != null) {
+        return res.status(400).json({ msg: "account already exist" })
+    }
+
+    await db.collection("users").insertOne({
+        name: name,
+        username: username, password
+    })
+    res.status(200).json({ name, email: username, password });
 
 }
 
