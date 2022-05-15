ff7e23bea36c20af38aa9d0598d6521727e9ed83
Lawrence Ephrim <ephrimlawrence@gmail.com>
2022-05-15 15:26:02 +0000

    user validation

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,13 +1,26 @@
 import { connectToDatabase } from "../../lib/mongodb"
+const Joi = require('joi');
 import bcrypt from "bcrypt"
 
 export default async function handler(req, res) {
     const {db} = await connectToDatabase()
 
     const {username, password} = req.body
-    // const password = req.body.password
 
-    const user = await db.collection("users").findOne({username: username})
+    const schema = Joi.object({
+        username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
+       password: Joi.string()
+       .pattern(new RegExp("^[a-zA-Z0-9]{3,30}"))
+    })
+    try{
+        await schema.validateAsync(req.body)
+
+        const user = await db.collection("users").findOne({username: username})
 
     if (user != null){
         if (await bcrypt.compare(password,user.password)){
@@ -19,4 +32,10 @@ export default async function handler(req, res) {
     }
 
    return res.status(400).json({ msg: 'invalid username or password' })
-  }
+
+    }catch(err){
+        res.status(400).json({msg: err.details[0].message});
+
+    }
+
+    }
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,25 +1,47 @@
 import { connectToDatabase } from "../../lib/mongodb";
 import bcrypt from "bcrypt"
-
+// import Joi from "joi"
+// import { Schema } from "joi";
+const Joi = require('joi');
 export default async function handler(req, res) {
     const { db } = await connectToDatabase()
 
-    const name = req.body.name
-    const username = req.body.username
-    const password = req.body.password
-
-    const user = await db.collection("users").findOne({ username: username })
-
-    if (user != null) {
-        return res.status(400).json({ msg: "account already exist" })
-    }
-
-    await db.collection("users").insertOne({
-        name: name,
-        username: username,
-        password: await bcrypt.hash(password, 10)
+    const { name, username, password } = req.body
+    const schema = Joi.object({
+        name: Joi.string()
+            .min(3)
+            .max(30)
+            .required(),
+        username: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(30)
+            .required(),
+        password: Joi.string()
+            .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+            repeat_password: Joi.ref("password"),
     })
-    res.status(200).json({ name, email: username, password });
+    //  t
+    try {
+
+        await schema.validateAsync(req.body)
+
+        const user = await db.collection("users").findOne({ username: username })
+
+        if (user != null) {
+            return res.status(400).json({ msg: "account already exist" })
+        }
+
+        await db.collection("users").insertOne({
+            name: name,
+            username: username,
+            password: await bcrypt.hash(password, 10)
+        })
+        res.status(200).json({ name, email: username, password });
+    } catch (err) {
+        res.status(400).json({msg: err.details[0].message});
+
+    }
 
 }
 
